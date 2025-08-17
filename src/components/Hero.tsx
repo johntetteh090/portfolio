5b31cc5a4214f6daea580c2e7debef4596dbbb8a
johntetteh090 <johntetteh090@gmail.com>
2025-08-17 15:56:57 +0000

    deployment test

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
   return (
     <section className="relative w-full bg-[#f8f7f2] pt-8 pb-10 overflow-hidden">
       {/* Desktop: image at bottom center */}
-      <div className="hidden md:block absolute bottom-[-9rem] sm:bottom-[-11.5rem] lg:bottom-[-8rem] xl:bottom-[-2.3] left-[17%] xl:left-[25%] right-0 z-0">
+      <div className="hidden md:block absolute bottom-[-9rem] sm:bottom-[-11.5rem] lg:bottom-[-8rem] xl:bottom-[-2.5rem] left-[17%] xl:left-[25%] right-0 z-0">
         <Image
           src="https://johntetteh090.github.io/portfolio/images/me/me.png"
           alt="Hero background"
